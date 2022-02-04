62962eae09cf3b0e1ab77655c0de7b33b4688b19
Jeff Workman <jworkhollr@aol.com>
2022-02-04 12:40:27 -0500

    Add project files.

---
+++ GeometricShapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes {
+    public class Rectangle {
+
+        public int Height { get; set; }
+        public int Witdh { get; set; }
+
+        public Rectangle(int height, int width) {
+            Height = height;
+            Witdh = width;
+            
+        }
+        public void PrintPerimeter() {
+            int perimeter = (Height + Witdh) * 2;
+
+            Console.WriteLine($"A rectangle with a height of {Height} and width of {Witdh}" +
+                $" has a permiter of {perimeter}");
+
+
+        }
+    }
+}
